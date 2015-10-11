51da015617d734ada1f8d04818a4ad20be2ca8fa
Sean Baquiro <sab437@nau.edu>
2015-10-11 14:50:27 -0700

    Basic navigation working

--- app/src/main/java/com/example/salazar32/teamfloyd_project2/Difficulty.java
+++ app/src/main/java/com/example/salazar32/teamfloyd_project2/Difficulty.java
@@ -1,7 +1,9 @@
 package com.example.salazar32.teamfloyd_project2;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 
 public class Difficulty extends AppCompatActivity {
@@ -10,5 +12,17 @@ public class Difficulty extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.difficulty);
+    }
+
+    public void clickEasy(View v) {
+        startActivity(new Intent(Difficulty.this, Gameplay.class));
+    }
+
+    public void clickMedium(View v) {
+        startActivity(new Intent(Difficulty.this, Gameplay.class));
+    }
+
+    public void clickHard(View v) {
+        startActivity(new Intent(Difficulty.this, Gameplay.class));
     }
 }
--- app/src/main/java/com/example/salazar32/teamfloyd_project2/MainActivity.java
+++ app/src/main/java/com/example/salazar32/teamfloyd_project2/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.salazar32.teamfloyd_project2;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +14,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home);
     }
+
+    public void clickPlay(View v) {
+        startActivity(new Intent(MainActivity.this, Difficulty.class));
+    }
+
+    public void clickCredits(View v) {
+        startActivity(new Intent(MainActivity.this, Credits.class));
+    }
+
 }
